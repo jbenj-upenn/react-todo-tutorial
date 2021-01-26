d86ad053ce271a0268159e3cdcb061e2e93e4ba2
Joshua Benjamin <jbenj@sas.upenn.edu>
2021-01-26 09:44:21 -0500

    information persists

--- src/App.js
+++ src/App.js
@@ -1,57 +1,62 @@
 import React, { useState, useRef, useEffect } from 'react';
-import TodoList from './TodoList'
-import uuidv4 from 'uuid/dist/v4'
+import TodoList from './TodoList';
+import uuidv4 from 'uuid/dist/v4';
+// import './App.css';
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef()
 
-  // save our todos to local storage with useEffect
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
+    // save our todos to local storage with useEffect
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if (storedTodos) setTodos(storedTodos)
+    }, [])
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
 
-  // this function allows us to toggle todos from complete to incomplete and vice versa
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
+    // this function allows us to toggle todos from complete to incomplete and vice versa
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
 
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
-    })
-    console.log(name)
-    // clears out the input field after adding the to-do
-    todoNameRef.current.value = null
-  }
+    function handleAddTodo(e) {
+        const name = todoNameRef.current.value
+        if (name === '') return
+        setTodos(prevTodos => {
+            return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+        })
+        console.log(name)
+            // clears out the input field after adding the to-do
+        todoNameRef.current.value = null
+    }
 
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
+    function handleClearTodos() {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
 
-  return (
-    <>
-      {/* below, we are passing props to the above 'todos' variable, just like we pass attributes to HTML elements */}
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add To-Do</button>
-      <button onClick={handleClearTodos}>Clear Completed To-Do</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
-  )
+    return ( <
+        > { /* below, we are passing props to the above 'todos' variable, just like we pass attributes to HTML elements */ } <
+        TodoList todos = { todos }
+        toggleTodo = { toggleTodo }
+        /> <
+        input ref = { todoNameRef }
+        type = "text" / >
+        <
+        button onClick = { handleAddTodo } > Add To - Do < /button> <
+        button onClick = { handleClearTodos } > Clear Completed To - Do < /button> <
+        div > { todos.filter(todo => !todo.complete).length }
+        left to do </div> <
+            />
+    )
 }
 
 export default App;
